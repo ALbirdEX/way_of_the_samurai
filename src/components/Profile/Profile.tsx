738518bf4d3330f5f587211be1e7c79fd8155895
ALbirdEX <Alex123890@gmail.com>
2023-12-07 00:11:45 +0300

    add Dialogs.tsx, Dialogs.module.css

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,10 +3,12 @@ import s from './Profile.module.css';
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             Main content
-            <img src={'https://www.vieribottazzini.com/wp-content/uploads/X1DII1_00190_slide.jpg'} alt={'content'}/>
+            <img className={s.images}
+                 src={'https://www.vieribottazzini.com/wp-content/uploads/X1DII1_00190_slide.jpg'}
+                 alt={'content'}/>
         </div>
         <div>
             ava + description
